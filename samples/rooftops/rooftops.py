2f8a0d0533690b0fcf1f6b487def95d608e08d62
niniack <nsa325@nyu.edu>
2020-08-23 16:43:16 +0400

    udpate config

--- samples/rooftops/rooftops.py
+++ samples/rooftops/rooftops.py
@@ -60,13 +60,13 @@ class RooftopsConfig(Config):
 
     # We use a GPU with 12GB memory, which can fit two images.
     # Adjust down if you use a smaller GPU.
-    IMAGES_PER_GPU = 1
+    IMAGES_PER_GPU = 2
 
     # Number of classes (including background)
     NUM_CLASSES = 1 + 11 # rooftops
 
     # Number of training steps per epoch
-    STEPS_PER_EPOCH = 60
+    STEPS_PER_EPOCH = 100
 
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
